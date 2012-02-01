40b2a93e61a2a7c59a64d5255bce3ac96e02eaa9
Hiroshi Kori <kori39@gmail.com>
2012-02-02 01:19:09 +0900

    test cases.

---
+++ testcases.js
@@ -0,0 +1,11 @@
+
+console.log(split("a b c d e f"));
+console.log(split(" b c d e "));
+console.log(split("  b  c  d  e  "));
+console.log(split("b  c  d  e"));
+console.log(split(" a b   c  d   e "));
+console.log(split("ab c de f gh i"));
+console.log(split(" ab c de f "));
+console.log(split("  a  bc  d  ef  "));
+console.log(split("ab  c  de  f"));
+console.log(split(" ab c   d  e   fg "));
