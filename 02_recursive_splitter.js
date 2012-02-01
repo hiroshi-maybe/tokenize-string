3b09d922062ff71d1625e390aa62ccae2ef06e57
Hiroshi Kori <kori39@gmail.com>
2012-02-01 09:19:31 +0900

    generated array was reversed, so modified.

--- 02_recursive_splitter.js
+++ 02_recursive_splitter.js
@@ -8,7 +8,7 @@ var split = function(input) {
     }
 
     if ( input.charAt(0) === " ") {
-      result.push(_recursive(input.substr(1,input.length-1), result));
+      result.unshift(_recursive(input.substr(1,input.length-1), result));
       return "";
     } else {
       return input.charAt(0)+_recursive(input.substr(1,input.length-1), result);
@@ -17,7 +17,7 @@ var split = function(input) {
 
   last_word = _rec(input, result);
   if (last_word !== " " && last_word !== "") {
-    result.push(last_word);
+    result.unshift(last_word);
   }
   return result;
 };
